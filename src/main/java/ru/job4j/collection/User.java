27f04ca920a9f2aecac29218522bb0842b888346
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-01-18 16:15:56 +0300

    Без переопределения equals и hashCode

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -12,22 +12,4 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
-
-    public static void main(String[] args) {
-        User user1 = new User("Vova", 2,
-                new GregorianCalendar(2000, Calendar.JANUARY, 1));
-        User user2 = new User("Vova", 2,
-                new GregorianCalendar(2000, Calendar.JANUARY, 1));
-
-        Map<User, Object> map = new HashMap<>();
-        map.put(user1, new Object());
-        map.put(user2, new Object());
-
-        for (Map.Entry<User, Object> entry : map.entrySet()) {
-            int h = entry.getKey().hashCode();
-            int hash = Objects.hash(h);
-            int i = hash & (16 - 1);
-            System.out.println("index = " + i);
-        }
-    }
 }
