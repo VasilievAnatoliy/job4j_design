d6566c0f70d6e175675fb56267a171afb8c4449b
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-01-26 15:36:24 +0300

    Анализ доступности сервера.

---
+++ src/test/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+            String start = null;
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] str = line.split(" ");
+                if (start == null && str[0].equals("400")|| str[0].equals("500")) {
+                    start = str[1];
+                }
+                if (start != null && str[0].equals("200") || str[0].equals("300")) {
+                    out.printf("%s%n", start + ";" + str[1] + ";");
+                    start = null;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analizy analizy = new Analizy();
+        analizy.unavailable("source.txt", "target.txt");
+    }
+}
