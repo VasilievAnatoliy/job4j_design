daec55e6cadfad27bd197ef72ae0b702fcf1cec0
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-01-28 14:20:39 +0300

    Тестирование IO

--- src/test/java/ru/job4j/io/AbuseTest.java
+++ src/test/java/ru/job4j/io/AbuseTest.java
@@ -1,14 +1,13 @@
 package ru.job4j.io;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class AbuseTest {
     @Rule
@@ -27,6 +26,6 @@ public class AbuseTest {
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
         }
-        assertThat(rsl.toString(), is("hello dude java job4j "));
+        assertEquals(rsl.toString(), "hello dude java job4j ");
     }
 }
