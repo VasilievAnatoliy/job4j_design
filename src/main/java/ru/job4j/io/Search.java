ab8378b9ad267e77ded00325ddbaf1910863f4e1
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-02-01 16:03:38 +0300

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,25 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        if (validation(args)) {
+            Path start = Paths.get(args[0]);
+            search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        }
+    }
+
+    public static Boolean validation(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Need root folder and file extension."
+                    + "Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Root folder not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Root folder not directory %s", file.getAbsoluteFile()));
+        }
+        return true;
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
