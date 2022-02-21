ff3c42f695fb58efe523f1ab3c116b44f77f4414
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-02-21 13:46:35 +0300

    Inner join

--- src/main/java/ru/job4j/sql/inner_join.sql
+++ src/main/java/ru/job4j/sql/inner_join.sql
@@ -1,3 +1,18 @@
+create table author(
+	id serial primary key,
+	name_author varchar(255)
+);
+
+create table book(
+	id serial primary key,
+	title varchar(255),
+	author_id int references author(id)
+);
+
+insert into author(name_author) values ('Булгаков М.А.');
+insert into book(title, author_id) values ('Мастер и Маргарита', 1);
+insert into book(title, author_id) values ('Белая гвардия', 1);
+
 select * from author as a
 join book as b
 on a.id = b.author_id;
