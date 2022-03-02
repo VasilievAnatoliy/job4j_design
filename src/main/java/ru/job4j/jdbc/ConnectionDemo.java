4728c526183e06bb7c56af7c25d965f62f64b65e
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-03-02 17:18:59 +0300

    JDBC

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -9,12 +9,12 @@ import java.sql.SQLException;
 
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Config config = new Config("app.properties");
+        Config config = new Config("./src/main/resources/app.properties");
         config.load();
-        Class.forName(config.value("hibernate.connection.driver_class"));
-        String url = config.value("hibernate.connection.url");
-        String login = config.value("hibernate.connection.username");
-        String password = config.value("hibernate.connection.password");
+        Class.forName(config.value("driver"));
+        String url = config.value("url");
+        String login = config.value("login");
+        String password = config.value("password");
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
             System.out.println(metaData.getUserName());
